f9d1153563d549861287171cb9ac70122fc1121a
Mahamad Gouse <mahammadgouse432@gmail.com>
2025-08-09 04:50:22 +0530

    Time: 0 ms (100%), Space: 43.1 MB (75.03%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int k = -1;
+
+        // Step 1: Find largest index k such that nums[k] < nums[k+1]
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                k = i;
+                break;
+            }
+        }
+
+        // If no such k exists, reverse the array
+        if (k == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        // Step 2: Find largest index l > k such that nums[k] < nums[l]
+        int l = -1;
+        for (int i = n - 1; i > k; i--) {
+            if (nums[i] > nums[k]) {
+                l = i;
+                break;
+            }
+        }
+
+        // Step 3: Swap nums[k] and nums[l]
+        swap(nums, k, l);
+
+        // Step 4: Reverse from k+1 to end
+        reverse(nums, k + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+}
